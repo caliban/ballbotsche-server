4dab154ee9ea1f37c6595af44531c105b94375d6
thp <>
2013-03-17 21:50:30 +0100

    work in progress on register / login

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -13,41 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-function RouteCtrl($route) {
-
-    var self = this;
-
-    $route.when('/register', {template:'tpl/register.html'});
-    $route.when('/index', {template:'index.html'});
-
-    $route.otherwise({redirectTo:'/index'});
-
-    $route.onChange(function () {
-        self.params = $route.current.params;
-    });
-
-    $route.parent(this);
-
+function LoginCtrl(Email) {
+ alert(Email); 
 }
-function LoginController($resource) {
-  $http({method: 'GET', url: '/someUrl'}).
-  success(function(data, status, headers, config) {
-    // this callback will be called asynchronously
-    // when the response is available
-  }).
-  error(function(data, status, headers, config) {
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
-  });
-  );
+LoginCtrl.$inject = ['Email'];
+function RegisterCtrl(player){
+    alert(player);
 }
- 
-BuzzController.prototype = {
-  fetch: function() {
-    this.activities = this.Activity.get({userId:this.userId});
-  },
-  expandReplies: function(activity) {
-    activity.replies = this.Activity.replies({userId:this.userId, activityId:activity.id});
-  }
-};
-BuzzController.$inject = ['$resource'];
+RegisterCtrl.$inject = ['player'];
