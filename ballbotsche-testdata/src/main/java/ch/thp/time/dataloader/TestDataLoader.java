a7c2374c98c4d5e57c5fdcc9315dbc07d2473ab6
Thierry Peng <thierry.peng@gmail.com>
2014-03-01 16:40:48 +0100

    reactivated testdataloader, added h2 sample testdatabase

--- ballbotsche-testdata/src/main/java/ch/thp/time/dataloader/TestDataLoader.java
+++ ballbotsche-testdata/src/main/java/ch/thp/time/dataloader/TestDataLoader.java
@@ -1,5 +1,19 @@
 /*
- * Copyright 2013 Thierry.
+ * Copyright 2014 Thierry.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *//*
+ * Copyright 2014 Thierry.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,9 +27,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package ch.thp.time.dataloader;
 
+import ch.thp.proto.ballbotsche.user.domain.Group;
+import ch.thp.proto.ballbotsche.user.domain.User;
+import ch.thp.proto.ballbotsche.user.domain.UserId;
+import com.google.common.collect.Sets;
+import java.util.UUID;
 import javax.annotation.PostConstruct;
 import javax.ejb.LocalBean;
 import javax.ejb.Singleton;
@@ -23,20 +41,24 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 /**
+ * Testsetup. poor mans database, aka a map in this case.
  *
  * @author Thierry
  */
 @Singleton
 @LocalBean
 public class TestDataLoader {
-    
+
     @PersistenceContext
-    private EntityManager em; 
-    
+    private EntityManager em;
+
     @PostConstruct
-    public void init()
-    {
-        
+    void init() {
+        Group applicationUsers = new Group(UUID.randomUUID().toString(), "ballbotscheUser", "can use the application");
+        em.persist(applicationUsers);
+        User userone = new User(new UserId(UUID.randomUUID().toString()), "ned.stark", "test1", Sets.newHashSet(applicationUsers));
+        User usertwo = new User(new UserId(UUID.randomUUID().toString()), "john.snow", "test2", Sets.newHashSet(applicationUsers));
+        em.persist(userone);
+        em.persist(usertwo);
     }
-    
 }
